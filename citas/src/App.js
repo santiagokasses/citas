0c7dacc9550d8d73dd4d55c9e392adcab65bd58d
CamilaAylenLopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-05-08 10:41:40 -0300

    a

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import cita from './componentes/Cita'
+import Cita from './componentes/Cita'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Cita titulos="ADMINISTRADOR DE PACIENTES">
         <Cita nro="1"/>
         <Cita nro="2"/>
         <Cita nro="3"/>
---
+++ citas/src/componentes/Cita.jsx
@@ -0,0 +1,15 @@
+const Cita = (props) => {
+    return(
+        <div>
+            hola: {props.nro}
+        </div>
+    )
+}
+const titulo = (props) => {
+    return(
+        <div>
+           <h1>{props.titulos}</h1> 
+        </div>
+    )
+}
+export default Cita; titulo;
