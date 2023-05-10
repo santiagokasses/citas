4b5e9994a640f8b4dc6f6ebb3e67167ec679725d
CamilaAylenLopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-05-10 08:29:05 -0300

    a

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Cita from './componentes/Cita'
+import Titulos from './componentes/Titulos'
+import Subtitulos from './componentes/Subtitulos'
+import Formulario from './componentes/Formulario'
 
 function App() {
   return (
@@ -7,17 +10,19 @@ function App() {
       <div className="App">
         <div id="root">
           <header className="App-header">
-            <Cita titulos="ADMINISTRADOR DE PACIENTES" />
+            <Titulos Titulos="ADMINISTRADOR DE PACIENTES" />
             <div class="container">
               <div class="row">
                 <div class="one-half column">
-                  <Subtitulos subtitulos="Crear mi Cita"/>
-                  <Formulario text=""/>
+                  <Subtitulos subtitulos="Crear mi Cita" />
+                  <Formulario text="" />
                   <Cita nro="1" />
                   <Cita nro="2" />
                   <Cita nro="3" />
                   <Cita nro="4" />
-                </div></div></div>
+                </div>
+              </div>
+            </div>
           </header>
         </div>
       </div>
