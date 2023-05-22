39ed67c194ac406ceb67542cc0b068d80b5e6b58
CamilaAylenLopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-05-22 10:32:43 -0300

    a

--- citas/src/componentes/Textos.jsx
+++ citas/src/componentes/Textos.jsx
@@ -2,6 +2,9 @@ import React from "react"
 
 const Textos = (props) => {
     const citas = props.cita;
+    const eliminar = (a) => {
+       citas.slice(a-1, a+1)
+    }
     return citas && citas.map((cita) => (
         <div>    
             <p>Mascota: <span>{cita.mascota}</span></p>
@@ -9,7 +12,7 @@ const Textos = (props) => {
             <p>Fecha: <span>{cita.Fecha}</span></p>
             <p>Hora: <span>{cita.Hora}</span></p>
             <p>Sintomas: <span>{cita.Sintomas}</span></p>
-            <button className="button elimnar u-full-width">Eliminar ×</button>
+            <button className="button elimnar u-full-width" onClick={eliminar(citas.indexOf(cita))}>Eliminar ×</button>
         </div>
     ))
 }
